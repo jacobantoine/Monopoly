68e8b7d789fc16a2adfc2fbbbcf89b13bb03a38d
hatcherbme <hatcherbme@aol.com>
2014-02-28 13:43:05 -0500

    Revert "Revert "Tried to get rid of Monopoly2 but has a bug""
    This reverts commit 4a979247f83b23c3dc433bd9daebefb1da51548c.

--- Monopoly2/src/RuntheGame.java
+++ Monopoly2/src/RuntheGame.java
@@ -1,12 +1,14 @@
 //Brian Hatcher Gary Donovich Jacob Antoine Cody Mathena
 
+import java.awt.BorderLayout;
 import java.io.IOException;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 //Class that runs the program
 
-public class RuntheGame extends JPanel
+public class RuntheGame
 {
 	
 	private static final long serialVersionUID = 1L;
@@ -14,7 +16,7 @@ public class RuntheGame extends JPanel
 	public static void main(String[] args) throws IOException 
 	{
 	         
-		 Monopoly2 board = new Monopoly2();              //Makes the board
+		 GameActions board = new GameActions();              //Makes the board
 		 board.setTitle("Monopoly");
 	     board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //Sets its size and display to true
 	     board.setSize(1100, 750);
